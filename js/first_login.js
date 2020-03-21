2ba217b476f4cbf355660db5426d7c6493bd86c4
ukaron <serious1nikos@gmail.com>
2020-03-21 17:36:58 +0300

    fl

--- js/first_login.js
+++ js/first_login.js
@@ -201,27 +201,3 @@ function fill_tags() {
                 }
             }})
 }
-
-window.onload = function() {
-    var startPos;
-    var geoOptions = {
-        enableHighAccuracy: true
-    };
-
-    var geoSuccess = function(position) {
-        startPos = position;
-        console.log(startPos.coords.latitude);
-        console.log(startPos.coords.longitude);
-        geo = startPos;
-    };
-
-    var geoError = function(error) {
-        console.log('Error occurred. Error code: ' + error.code);
-
-        //   0: unknown error
-        //   1: permission denied
-        //   2: position unavailable (error response from location provider)
-        //   3: timed out
-    };
-    navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
-};
