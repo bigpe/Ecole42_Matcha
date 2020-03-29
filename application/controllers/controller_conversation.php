68cff9728c472893bb7c15be3ba2b35ddbfe38b4
ukaron <serious1nikos@gmail.com>
2020-03-29 18:34:12 +0300

    chat

--- application/controllers/controller_conversation.php
+++ application/controllers/controller_conversation.php
@@ -1,10 +1,12 @@
 <?php
+define('PORT', "1488");
 class Controller_Conversation extends Controller
 {
     function __construct()
     {
         $this->model = new Model_Conversation();
         $this->view = new View();
+
     }
 
     function action_index(){
@@ -22,13 +24,25 @@ class Controller_Conversation extends Controller
             header("Location: /");
     }
     function action_chat_view(){
-        if($this->check_get_arguments_exists(array("id"))){ #Success
-            $user_data = $this->model->get_chat_data($_GET['id']);
-            $this->view->generate("chat_view.php", "template_view.php",
-                array("error" => $this->model->error_handler($this->model->error_id),
-                    "users_data" => $user_data));
+        if($this->model->check_session()) { #Success
+            if ($this->check_get_arguments_exists(array("id"))) { #Success
+                $user_data = $this->model->get_chat_data($_GET['id']);
+                $this->view->generate("chat_view.php", "template_view.php",
+                    array("error" => $this->model->error_handler($this->model->error_id),
+                        "users_data" => $user_data));
+            }
+            else
+                header("Location: /");
         }
         else
             header("Location: /");
+    }
+
+
+    function action_save_message(){
+        if ($this->check_post_arguments_exists(array("user_from", "user_to", "message", "type"))){
+            if ($_POST['type'] == 1)
+                $this->model->save_message($_POST['user_from'], $_POST['user_to'], $_POST['message']);
+        }
     }
 }
---
+++ ws/server.php
@@ -0,0 +1,43 @@
+<?php
+require_once('chat.php');
+$chat = new Chat();
+$null = null;
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if (!is_resource($socket))
+    echo 'Не могу создать сокет: '. socket_strerror(socket_last_error()) . PHP_EOL;
+if (!socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1))
+    echo 'Не могу установить опцию на сокете: '. socket_strerror(socket_last_error()) . PHP_EOL;
+if (!socket_bind($socket, 0, 8888))
+    echo 'Не могу привязать сокет: '. socket_strerror(socket_last_error()) . PHP_EOL;
+if(!socket_listen($socket))
+    echo "error listen";
+$clientSocketArray = array($socket);
+while (true){
+    $newSocketArray = $clientSocketArray;
+    socket_select($newSocketArray, $null, $null, 0, 10);
+    if (in_array($socket, $newSocketArray)){
+        $newSocket = socket_accept($socket);
+        $clientSocketArray[] = $newSocket;
+        $header = socket_read($newSocket, 1024);
+        $chat->sendHeaders($header, $newSocket, '192.168.0.191/ws', 8888);
+        $newSocketIndex = array_search($socket, $newSocketArray);
+        unset($newSocketArray[$newSocketIndex]);
+    }
+    foreach ($newSocketArray as $newSocketArrayResource){
+        while (socket_recv($newSocketArrayResource, $socketData, 1024, 0 ) >= 1){
+            $socketMessage = $chat->unseal($socketData);
+            $messageObj = json_decode($socketMessage);
+            $chat_box_message = $chat->createChatBoxMessage($messageObj->user_from, $messageObj->user_to, $messageObj->message,
+                                                            $messageObj->type);
+            $chat->send($chat_box_message);
+            break 2;
+        }
+        $socketData = @socket_read($newSocketArrayResource, 1024, PHP_NORMAL_READ);
+        if ($socketData === false){
+            $newSocketIndex = array_search($newSocketArrayResource, $clientSocketArray);
+            unset($clientSocketArray[$newSocketIndex]);
+        }
+    }
+}
+socket_close($socket);
+?>
