5c6f6e1d364e82a43c2ddaaa1b173cfefe498b09
ukaron <serious1nikos@gmail.com>
2020-04-09 12:20:40 +0300

    fb

--- application/views/auth_view.php
+++ application/views/auth_view.php
@@ -13,3 +13,27 @@
     <a href="/registration/">Sign Up</a>
     <a href="/registration/pre_restore_password">Restore password</a>
 </div>
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/ru_RU/sdk.js#xfbml=1&autoLogAppEvents=1&version=v6.0&appId=821000615077304"></script>
+<div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '{821000615077304}',
+            cookie     : true,
+            xfbml      : true,
+            version    : 'v6.0'
+        });
+
+        FB.AppEvents.logPageView();
+
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
