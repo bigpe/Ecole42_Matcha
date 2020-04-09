4f97e6b4fcea0c881db2a9c716302e4f2e60fe17
ukaron <serious1nikos@gmail.com>
2020-04-09 20:42:10 +0300

    fb

--- application/views/auth_view.php
+++ application/views/auth_view.php
@@ -1,3 +1,4 @@
+<?php include '/config/config.php'?>
 <div style="display: grid;
     min-height: calc(100vh - 69px - 100px - 60px);
     align-items: center;
@@ -12,7 +13,7 @@
     </form>
     <a href="/registration/">Sign Up</a>
     <a href="/registration/pre_restore_password">Restore password</a>
+    <a href='https://www.facebook.com/v6.0/dialog/oauth?client_id=821000615077304&redirect_uri=https://matcha.fun/Auth/with_FB&state=bda7da561adf35d16563e653e60df8cb'>
+        Join with FB
+    </a>
 </div>
-<div id="fb-root"></div>
-<div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default"
-     data-auto-logout-link="false" data-use-continue-as="false"></div>
